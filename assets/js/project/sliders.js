dc423036939eaf97131e014dcf444b7824755c3e
yash <ybv1496@gmail.com>
2022-06-12 14:25:34 +0530

    First Commit

---
+++ assets/js/project/sliders.js
@@ -0,0 +1,18 @@
+﻿$(window).load(function () {
+    $('.basic').flexslider({
+        animation: "slide"
+    });
+    $('.simple-caption').flexslider({
+        animation: "slide"
+    });
+    $('.thumbnail-img-control').flexslider({
+        animation: "slide",
+        controlNav: "thumbnails"
+    });
+    $('.basic-carousel').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 210,
+        itemMargin: 5
+    });
+});
---
+++ assets/js/project/text-rotator.js
@@ -0,0 +1,30 @@
+﻿$("#rotate-fade-in").Morphext({
+    animation: "fadeIn",
+    separator: ",",
+    speed: 3000,
+});
+$("#rotate-bounce-in").Morphext({
+    animation: "bounceIn",
+    separator: ",",
+    speed: 3000,
+});
+$("#rotate-shake").Morphext({
+    animation: "shake",
+    separator: ",",
+    speed: 3000,
+});
+$("#rotate-tada").Morphext({
+    animation: "tada",
+    separator: ",",
+    speed: 3000,
+});
+$("#rotate-wobble").Morphext({
+    animation: "wobble",
+    separator: ",",
+    speed: 3000,
+});
+$("#rotate-jello").Morphext({
+    animation: "jello",
+    separator: ",",
+    speed: 3000,
+});
